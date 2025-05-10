ea188ca0e231247af7fc062b635cd6b51f090250
francoandresriggio <friggio.87@gmail.com>
2025-05-10 13:32:12 -0300

    feat(access): Authorize production to access to API

--- api/products/index.ts
+++ api/products/index.ts
@@ -6,7 +6,13 @@ import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "http-status-codes";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   // Manejo de CORS
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Cambia según sea necesario
+  const allowedOrigins = ["http://localhost:3000", "https://francoandresriggio.github.io"];
+  const origin = req.headers.origin || "";
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); // Permitir métodos
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Permitir encabezados
 
